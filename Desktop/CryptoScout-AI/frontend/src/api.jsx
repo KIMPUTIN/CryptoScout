e2ed4d11e485b41ce6de6e7b7e2eb491809169a3
Kelvin Kay <webappsforfun@gmail.com>
2026-02-05 14:40:54 +0200

    Removed hardcoded url in frontend/src/api.jsx

--- Desktop/CryptoScout-AI/frontend/src/api.jsx
+++ Desktop/CryptoScout-AI/frontend/src/api.jsx
@@ -1,5 +1,7 @@
 
-const API_BASE = "https://cryptoscout-production.up.railway.app";
+//const API_BASE = "https://cryptoscout-production.up.railway.app";
+const API_BASE = import.meta.env.VITE_API_URL;
+
 
 console.log("API TEST VERSION 2"); // <--- ADD THIS
 
